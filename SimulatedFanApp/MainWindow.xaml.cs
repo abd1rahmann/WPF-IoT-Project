c3dfc5335528fba6a9bcecee37c569d800605c27
abd1rahmann <abdi123rahman@outlook.com>
2024-10-07 12:54:00 +0200

    Add project files.

---
+++ SimulatedFanApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimulatedFanApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool IsRunning = false; 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TopWindowBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IsRunning = !IsRunning;
+
+            var sb = (BeginStoryboard)TryFindResource("rotate-sb");
+
+            if(IsRunning)
+                sb.Storyboard.Begin();
+            else
+                sb.Storyboard.Stop();
+        }
+    }
+}
